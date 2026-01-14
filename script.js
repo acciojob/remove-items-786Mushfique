dfa6193fc719054c26c9d731106704fe54adfd4b
Md Mushfique Raza <114329904+786Mushfique@users.noreply.github.com>
2026-01-14 11:17:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+var select = document.getElementById('colorSelect');
+
+
+function removecolor() {
+  const opt  = select.options[select.selectedIndex];
+  if (opt) opt.remove()
+}
